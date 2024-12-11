23c86c66f770df5fa9a509ab92f6abf1565fc20f
Ahmed Khaled <ahmed.khaledswaby@gmail.com>
2024-12-11 02:40:10 +0200

    #feat: handle right click

--- src/Components/Directory.tsx
+++ src/Components/Directory.tsx
@@ -3,31 +3,38 @@ import './Directory.scss';
 import { File , Copy , Delete , Rename , Folder} from '../Assets/icons';
 const Directory = ({ files }) => {
     const [active, setActive] = useState(false)
+    const [activeActions, setActiveAction] = useState(false)
 
     const activeState = () => {
         setActive(!active)
-      
+    }
 
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        setActiveAction(!activeActions)
     }
 
     if (files.type === 'file') {
         return (
             <>
                 <div className="btn file" data-collapsed={active}>
-                    <File />
-                    {files.name}
-                    <div className="action_wrapper">
-                        <button className="btn" onClick={() => alert('copied success!')}>
-                            <Copy />
-                        </button>
-                        <button className="btn" onClick={() => alert('deleted success!')}>
-                            <Delete />
-                        </button>
-                        <button className="btn" onClick={() => alert('renamed success!')}>
-                            <Rename />
-                        </button>
-
-                    </div>
+                    <button className='btn' onContextMenu={(e) => handleRightClick(e)}>
+                        <File />
+                        {files.name}
+                    </button>
+                    {activeActions && (
+                        <div className="action_wrapper">
+                            <button className="btn" onClick={() => alert('copied success!')}>
+                                <Copy />
+                            </button>
+                            <button className="btn" onClick={() => alert('deleted success!')}>
+                                <Delete />
+                            </button>
+                            <button className="btn" onClick={() => alert('renamed success!')}>
+                                <Rename />
+                            </button>
+                        </div>
+                    )}
                     
                     </div>
             </>
